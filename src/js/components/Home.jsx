001278e2986d87868dea59908fbee7bb893d60bb
LoretoConcha <109807122+LoretoConcha@users.noreply.github.com>
2025-07-28 22:58:34 +0000

    first to do list

--- src/js/components/Home.jsx
+++ src/js/components/Home.jsx
@@ -1,26 +1,66 @@
-import React from "react";
+import React, { useState } from "react";
 
-//include images into your bundle
-import rigoImage from "../../img/rigo-baby.jpg";
 
 //create your first component
 const Home = () => {
-	return (
-		<div className="text-center">
-            
 
-			<h1 className="text-center mt-5">Hello Rigo!</h1>
-			<p>
-				<img src={rigoImage} />
-			</p>
-			<a href="#" className="btn btn-success">
-				If you see this green button... bootstrap is working...
-			</a>
-			<p>
-				Made by{" "}
-				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
-				love!
-			</p>
+	const [task, setTask] = useState("");
+	const [tasks, setTasks] = useState([]);
+	const deleteTask = (indexToDelete) => {
+		setTasks(tasks.filter((_, index) => index !== indexToDelete));
+	}
+
+
+	return (
+		<div className="container">
+
+			<h1 className="text-center mt-5" style={{ color: 'lightpink', fontWeight: '300' }}>todos</h1>
+			<ul className="list-group mt-3">
+
+				<li className="list-group-item">
+					<input
+						type="text"
+						className="form-control"
+						placeholder="What needs to be done?"
+						value={task}
+						onChange={(e) => {
+							setTask(e.target.value)
+							console.log("On change: ", e.target.value);
+
+						}}
+						onKeyDown={(e) => {
+							console.log("onKeyDown: ", e.key);
+							if (e.key === "Enter" && task.trim() != "") {
+								setTasks([...tasks, task]);
+								setTask("");
+							}
+						}}
+
+					/>
+				</li>
+
+				{tasks.length === 0 ? (
+					<li className="list-group-item">No tasks, add a task</li>
+				) : (
+					tasks.map((t, index) => (
+						<li
+							key={index}
+							className="list-group-item d-flex justify-content-between align-items-center"
+						>
+							{t}
+							<span
+								className="delete-btn text-danger"
+								onClick={() => deleteTask(index)}
+								style={{ cursor: "pointer" }}
+							>
+								X
+							</span>
+						</li>
+					))
+				)}
+			</ul>
+
+
 		</div>
 	);
 };
